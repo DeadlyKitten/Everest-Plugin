c280bb91d6531a3e648b84c3c1906231dcf1254a
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2025-07-12 01:21:08 -0700

    Add william accessory.

---
+++ Everest Plugin/Accessories/WilliamAccessory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Everest.Accessories
+{
+    public class WilliamAccessory : MonoBehaviour
+    {
+        [SerializeField]
+        private Material _material;
+
+#if PLUGIN
+        private void Start()
+        {
+            var renderer = GetComponentInParent<SkinnedMeshRenderer>();
+            if (renderer) renderer.material = _material;
+        }
+#endif
+    }
+}
